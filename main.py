f5c0343447c7e78d50355c9f020de4cb9957e8c0
LeeDS <ehdtn3540@gmail.com>
2025-12-11 23:50:10 +0900

    up, down game 테스트

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+from fastapi.responses import JSONResponse
 
 @app.get("/")
 def read_root():
@@ -9,6 +10,16 @@ def read_root():
 @app.get("/test")
 def test():
     return {"message": "Hello FastAPI! test"}
+
+# guess number up, down game
+SECRET = 42
+@app.get("/guess")
+def guess_number(num: int):
+    if num < SECRET:
+        return {"result": "UP!"}
+    elif num > SECRET:
+        return {"result": "DOWN!"}
+    return JSONResponse(content={"result": "정답!"}, media_type="application/json; charset=utf-8") # 한글 깨짐 방지 처리
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
