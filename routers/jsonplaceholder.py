e24f1c21f46fb472969b715ef95474f652f66712
LeeDS <ehdtn3540@gmail.com>
2026-03-07 19:05:14 +0900

    jsonplaceholder의 posts, comments, user 데이터 조합 작업

--- routers/jsonplaceholder.py
+++ routers/jsonplaceholder.py
@@ -18,3 +18,16 @@ async def get_comments():
 async def get_users():
     response = await http_client.client.get("https://jsonplaceholder.typicode.com/users")
     return response.json()
+
+@router.get("/combined_posts")
+async def get_combined_posts():
+    posts    = await get_posts()
+    comments = await get_comments()
+    users    = await get_users()
+
+    combined_list = []
+    combined_list.append(posts[0])
+    combined_list.append(comments[0])
+    combined_list.append(users[0])
+
+    return combined_list
