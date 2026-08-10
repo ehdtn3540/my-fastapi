9918981576f4c9c300433242292601625bb483e3
LeeDS <ehdtn3540@gmail.com>
2026-08-10 09:26:23 +0900

    프로젝트에 맞지 않는 파일 삭제 및 코드 수정

--- routers/system.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/system", tags=[""])
-
-@router.get("/health")
-async def test_connection():
-    return {"status": "success", "message": "FastAPI와 연결되었습니다!"}
-
